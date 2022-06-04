1bead70e73024589b5e7919c1e25205efd338e29
JavierGuerrero9 <102624992+JavierGuerrero9@users.noreply.github.com>
2022-06-03 22:27:34 -0500

    Add files via upload

---
+++ Practice_8.X/build/default/production/_ext/1472/LCD_disp_Driver.p1.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/1472/LCD_disp_Driver.p1:  \
+../LCD_disp_Driver.c  \
+../LCD_disp_Driver.h  \
+../device_config.h 
---
+++ Practice_8.X/build/default/production/main_8.p1.d
@@ -0,0 +1,4 @@
+build/default/production/main_8.p1:  \
+main_8.c  \
+device_config.h  \
+LCD_disp_Driver.h 
